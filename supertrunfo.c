56997f23e34542cabf087eb427679fb64f95afa1
everthonpontel <everthonpontel14@gmail.com>
2025-04-28 02:47:55 +0000

    primeiro desenvolvimento carta1

---
+++ supertrunfo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int carta = 1;
+    int populacao = 11.5000;
+    
+    printf("carta: %d",carta);
+    scanf("%d",&carta); 
+    printf("populacao: %d",populacao);
+    scanf("%d",&populacao);
+    
+
+return 0;
+
+
+
+
+}
