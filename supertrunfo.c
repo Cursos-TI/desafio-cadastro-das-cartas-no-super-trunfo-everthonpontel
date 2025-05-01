1de642af85fc9d36cf3a7516b04b5bc600330c26
everthonpontel <everthonpontel14@gmail.com>
2025-05-01 22:42:26 +0000

    carata 1 e carta 2

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    int carta = 1;
-    int populacao = 11.5000;
+     //carta 1
+    // valores fixos 
+    int populacao = 110000;
+    float area = 24.000;
+    float pib = 200.0000;
+    int pontos_turisticos = 120;
+
+    //  título
+    printf("Carta 1\n");
+
     
-    printf("carta: %d",carta);
-    scanf("%d",&carta); 
-    printf("populacao: %d",populacao);
-    scanf("%d",&populacao);
+    printf("\nSão Paulo \n");
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);  
+    printf("PIB: R$ %.2f bilhões\n", pib);  
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+    printf("=====================\n\n");
+    
+    //carta 2
+    // valores fixos
+    int populacao_ = 80000;
+    float area_ = 43.000;
+    float pib_ = 250.0000;
+    int pontos__turisticos = 110;
+
+     //titulo
+    printf("Carta 2\n");
+
+
+    printf("\nRio De Janeiro \n");
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);  
+    printf("PIB: R$ %.2f bilhões\n", pib);  
+    printf("Pontos turísticos: %d\n", pontos__turisticos);
     
 
-return 0;
 
 
-
-
+    return 0;
 }
